3df0024952fde443986c36f1c66148c1fb9d5c0f
joyTsing <792566033@qq.com>
2020-12-25 15:58:42 +0800

    init

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+/* 65536 locations */
+uint16_t memory[UINT16_MAX];
+
+int main()
+{
+
+    return 0;
+}
+
