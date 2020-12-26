56c02df63f4bfabc9fbb106afb0f428e63eadd01
joyTsing <792566033@qq.com>
2020-12-26 16:21:38 +0800

    添加trap code

--- main.c
+++ main.c
@@ -120,6 +120,16 @@ enum{
     FL_NEG=1<<2,    /* N */
 };
 
+/* trap codes */
+enum{
+    TRAP_GETC = 0x20,  /* get character from keyboard */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* input a string */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
 
 uint16_t sign_extend(uint16_t x,int bit_count){
     if((x>>(bit_count-1))&1){
