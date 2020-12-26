e3c274b6ba3087b9496b307d8396eda29ea967c7
joyTsing <792566033@qq.com>
2020-12-26 19:30:37 +0800

    存储器映射寄存器

--- main.c
+++ main.c
@@ -37,7 +37,6 @@ void restore_input_buffering(){
 #include <sys/types.h>
 #include <sys/termios.h>
 #include <sys/mman.h>
-
 /** Platform Specifics **/
 
 /* get keyboard status */
@@ -130,6 +129,15 @@ enum{
     TRAP_HALT = 0x25   /* halt the program */
 };
 
+/* memory mapped registers */
+enum{
+    MR_KBSR=0xFE00, /* keyboard status */
+    MR_KBDR=0xFE02 /* keyboard data */
+};
+
+uint16_t swap16(uint16_t x){
+    return (x<<8)|(x>>8);
+}
 
 uint16_t sign_extend(uint16_t x,int bit_count){
     if((x>>(bit_count-1))&1){
@@ -138,14 +146,58 @@ uint16_t sign_extend(uint16_t x,int bit_count){
     return x;
 }
 
+void mem_write(uint16_t address,uint16_t val){
+    memory[address]=val;
+}
+
+uint16_t mem_read(uint16_t address){
+    /* reading the memory mapped keyboard register triggers a key check */
+    if(address==MR_KBSR){
+        if(check_key()){
+            memory[MR_KBSR]=(1<<15);
+            memory[MR_KBDR]=getchar();
+        }else{
+            memory[MR_KBSR]=0;
+        }
+    }
+    return memory[address];
+}
+
 void update_flags(uint16_t r){
     if(reg[r]==0){
-        reg[R_COND]==FL_ZRO;
+        reg[R_COND]=FL_ZRO;
     }else if(reg[r]>>15){
         reg[R_COND]=FL_NEG;
     }else{
         reg[R_COND]=FL_POS;
     }
+}
+
+void read_image_file(FILE* file){
+    /* origin tells us where in memory to place the image */
+    uint16_t origin;
+    fread(&origin,sizeof(origin),1,file);
+    origin=swap16(origin);
+
+    /* we know the maximum file size so we only need one fread */
+    uint16_t max_read=UINT16_MAX-origin;
+    uint16_t* p=memory+origin;
+    size_t read=fread(p,sizeof(uint16_t),max_read,file);
+
+    /* swap to little endian */
+    while(read-- >0){
+        *p=swap16(*p);
+        ++p;
+    }
+
+}
+
+int read_image(const char* image_path){
+    FILE* file=fopen(image_path,"rb");
+    if(!file){return 0;}
+    read_image_file(file);
+    fclose(file);
+    return 1;
 }
 
 /* execute trap routine */
